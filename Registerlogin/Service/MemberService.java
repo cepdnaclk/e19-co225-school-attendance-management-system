5ad555ceac654d3b240a1a9c158660cc13841b40
Thevambikai <110016117+Thevambikai@users.noreply.github.com>
2023-06-15 17:04:52 +0530

    Delete Registerlogin directory

--- Registerlogin/Service/MemberService.java
+++
@@ -1,7 +0,0 @@
-package webapplication.Registerlogin.Service;
-
-import webapplication.Registerlogin.Dto.MemberDTO;
-
-public interface MemberService {
-    String addMember(MemberDTO memberDTO);
-}
