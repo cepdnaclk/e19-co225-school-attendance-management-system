90e3cfbef505b704edd7fe81ad32f49ab91bf581
Thevambikai <110016117+Thevambikai@users.noreply.github.com>
2023-06-24 10:12:08 +0530

    Add pages

--- Teacher/src/index.js
+++ Teacher/src/index.js
@@ -3,13 +3,43 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AddStudent  from './Pages/AddStudent'
+import MarkAttendance  from './Pages/MarkAttendance'
+import Parent from './Pages/Parent'
+import View from './Pages/ViewStatus'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "addstudent",
+    element: <AddStudent/>,
+  },
+  {
+    path: "markattendance",
+    element: <MarkAttendance/>,
+  },
+  {
+    path: "parent",
+    element: <Parent/>,
+  },
+  {
+    path: "viewstatus",
+    element: <View/>,
+  }
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  root.render(
+    <RouterProvider router={router} />
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
