2c6fe81d33f93d718badbf6f6522cf0b4e4e4bb9
Danujan <sivasundaralingamdanujan@gmail.com>
2023-06-29 16:30:58 +0530

    Mark attendance front end page changed

--- Teacher/demo/src/main/java/com/FullStackApplication/demo/exception/AttendeeNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.FullStackApplication.demo.exception;
-
-import com.FullStackApplication.demo.model.Attendee;
-
-public class AttendeeNotFoundException extends RuntimeException{
-    public AttendeeNotFoundException(Long id){
-        super ("Could not found teh attendee with if "+ id);
-    }
-}
--- Teacher/demo/src/main/java/com/FullStackApplication/demo/repository/studentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.FullStackApplication.demo.repository;
-
-import com.FullStackApplication.demo.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface studentRepository extends JpaRepository<Student,Long> {
-}
