a395d2cf1690f71442f22d9e59b42bbb68290cba
Nithusikan01 <115540028+Nithusikan01@users.noreply.github.com>
2023-07-03 02:02:47 +0530

    AttendanceController.java updated with new features

--- Authoritative Member/AbsentPresentCount/AttendanceController.java
+++ Authoritative Member/AbsentPresentCount/AttendanceController.java
@@ -1,12 +1,16 @@
 <<<<<<< Updated upstream
 package com.ntxcodes.attendancecount.controller;
 
+import com.ntxcodes.attendancecount.model.Attendance;
+import com.ntxcodes.attendancecount.repository.AttendanceCountRepository;
 import com.ntxcodes.attendancecount.service.AttendanceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 @RequestMapping("/api/attendance")
