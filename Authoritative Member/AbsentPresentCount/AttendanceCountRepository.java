3d40bbe938718cb8c2256cfb9605082630396cef
Nithusikan01 <115540028+Nithusikan01@users.noreply.github.com>
2023-07-03 02:07:16 +0530

    Updated AttendanceCountRepository.java with new features

--- Authoritative Member/AbsentPresentCount/AttendanceCountRepository.java
+++ Authoritative Member/AbsentPresentCount/AttendanceCountRepository.java
@@ -7,15 +7,9 @@ import com.ntxcodes.attendancecount.model.Attendance;
 public interface AttendanceCountRepository extends JpaRepository<Attendance, Integer> {
     // Repository methods
     long countByAttendanceStatus(boolean attendanceStatus);
-}
-=======
-package com.ntxcodes.attendancecount.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.ntxcodes.attendancecount.model.Attendance;
+    int countByGradeAndAttendanceStatus(String grade, boolean attendanceStatus);
 
-public interface AttendanceCountRepository extends JpaRepository<Attendance, Integer> {
-    // Repository methods
-    long countByAttendanceStatus(boolean attendanceStatus);
+    Attendance findByStudentId(int studentId);
 }
 >>>>>>> Stashed changes
